9ccb1c208f53b27f693616c6bf1da4d9eb5da3d5
Ruhani05 <147978993+Ruhani05@users.noreply.github.com>
2024-03-09 23:41:59 +0530

    Update U-type.py

--- U-type.py
+++ U-type.py
@@ -1,17 +1,21 @@
 def decimal_to_twos_complement(decimal_num):
     if decimal_num < 0:
         abs_decimal = abs(decimal_num)
-        complement = 2**21 - abs_decimal
+        complement = 2**32 - abs_decimal
     else:
         complement = decimal_num
 
     twos = bin(complement)[2:]
     
-    if len(twos) < 21:
-        twos = '0' * (21 - len(twos)) + twos
+    if len(twos) < 32:
+        twos = '0' * (32 - len(twos)) + twos
 
     return twos
     
 
-def lw(l, d):
-    return decimal_to_twos_complement(int(l[2])) + d[l[3].rstrip(")")] + "010" + d[l[1]] + "0000011"
+def auipc(l, d):
+    x=decimal_to_twos_complement(int(l[2]))
+    return x[-32:-12:] + "0010111"
+def lui(l, d):
+    x=decimal_to_twos_complement(int(l[2]))
+    return x[-32:-12:] + "0110111" 
