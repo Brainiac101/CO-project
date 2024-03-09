948a974942d5809e0e2cbc54d4fef18ed9456fe7
Revant Gupta <147755345+Brainiac101@users.noreply.github.com>
2024-03-09 13:47:29 +0530

    R-type.py

--- R-type.py
+++ R-type.py
@@ -14,31 +14,31 @@ def split(string):
     return l;
 
 def add(l, dict):
-    return "1100110 " + dict[l[1]] + " 000 " + dict[l[2]] + " " + dict[l[3]] + " 0000000";
+    return "0000000" + dict(l[3]) + dict[l[2]] + "000" + dict[l[1]] + "0110011";
 
 def sub(l, dict):
-    return "1100110 " + dict[l[1]] + " 000 " + dict[l[2]] + " " + dict[l[3]] + " 0000010";
-
+    return "0100000" + dict[l[3]] + dict[l[2]] + "000" + dict[l[1]] + "0110011";
+    
 def sll(l, dict):
-    return "1100110 " + dict[l[1]] + " 100 " + dict[l[2]] + " " + dict[l[3]] + " 0000000";
+    return "0000000" + dict[l[3]] + dict[l[2]] + "001" + dict[l[1]] + "0110011";
 
 def slt(l, dict):
-    return "1100110 " + dict[l[1]] + " 010 " + dict[l[2]] + " " + dict[l[3]] + " 0000000";
+    return "0000000" + dict[l[3]] + dict[l[2]] + "010" + dict[l[1]] + "0110011";
 
 def sltu(l, dict):
-    return "1100110 " + dict[l[1]] + " 110 " + dict[l[2]] + " " + dict[l[3]] + " 0000000";
+    return "0000000" + dict[l[3]] + dict[l[2]] + "011" + dict[l[1]] + "0110011";
 
 def xor(l, dict):
-    return "1100110 " + dict[l[1]] + " 001 " + dict[l[2]] + " " + dict[l[3]] + " 0000000";
+    return "0000000" + dict[l[3]] + dict[l[2]] + "100" + dict[l[1]] + "0110011";
 
 def srl(l, dict):
-    return "1100110 " + dict[l[1]] + " 101 " + dict[l[2]] + " " + dict[l[3]] + " 0000000";
+    return "0000000" + dict[l[3]] + dict[l[2]] + "101" + dict[l[1]] + "0110011";
 
 def ory(l, dict):
-    return "1100110 " + dict[l[1]] + " 011 " + dict[l[2]] + " " + dict[l[3]] + " 0000000";
+    return "0000000" + dict[l[3]] + dict[l[2]] + "110" + dict[l[1]] + "0110011";
 
 def andy(l, dict):
-    return "1100110 " + dict[l[1]] + " 111 " + dict[l[2]] + " " + dict[l[3]] + " 0000000";
+    return "0000000" + dict[l[3]] + dict[l[2]] + "111" + dict[l[1]] + "0110011";
 
 dict={"zero" : "00000", "ra":"00001", "sp": "00010", "gp":"00011", "tp":"00100", "t0":"00101", "t1":"00110", "t2":"00111", "s0":"01000", "fp":"01000", "s1":"01001", "a0":"01010", "a1":"01011", "a2": "01100", "a3":"01101", "a4":"01110", "a5":"01111", "a6":"10000", "a7":"10001", "s2":"10010", "s3":"10011", "s4": "10100", "s5":"10101", "s6":"10110", "s7":"10111", "s8":"11000", "s9":"11001", "s10": "11010", "s11":"11011", "t3": "11100", "t4":"11101", "t5":"11110", "t6":"11111"}
 fin = open("sample.txt", "r")
