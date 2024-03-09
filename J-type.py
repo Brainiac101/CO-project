1f5eae4af7101cdc015dd29d7053a692254c64ee
Ruhani05 <147978993+Ruhani05@users.noreply.github.com>
2024-03-10 01:15:31 +0530

    Update J-type.py

--- J-type.py
+++ J-type.py
@@ -1,4 +1,4 @@
-def decimal_to_twos_complement(decimal_num):
+def decimal_to_twos_complement_12(decimal_num):
     if decimal_num < 0:
         abs_decimal = abs(decimal_num)
         complement = 2**21 - abs_decimal
@@ -15,6 +15,6 @@ def decimal_to_twos_complement(decimal_num):
 
 def jal(l, d):
     if l[2] in labels:
-        x=decimal_to_twos_complement(pc-labels[l[2]])
-    else:x=decimal_to_twos_complement(int(l[2]))
+        x=decimal_to_twos_complement_12(pc-labels[l[2]])
+    else:x=decimal_to_twos_complement_12(int(l[2]))
     return x[0]+x[-11:-1:] + x[-12]+ x[-20:-12:]+ d[l[1]] + "1101111" 
