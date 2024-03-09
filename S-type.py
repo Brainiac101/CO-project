b7b64e39e81e33560eafd09a7f87526dcaf5803f
Jaitrika <141649577+Jaitrika@users.noreply.github.com>
2024-03-09 22:30:00 +0530

    Update S-type.py

--- S-type.py
+++ S-type.py
@@ -12,6 +12,6 @@ def decimal_to_twos_complement(decimal_num):
 
     return twos
 
-def sw(l, dict):
+def sw(l, d):
     imm=decimal_to_twos_complement(int(l[2]))
-    return imm[:7]+ dict[l[1]] + dict[l[3].rstrip(")")] + "010" + imm[7:] + "0100011"
+    return imm[:7]+ d[l[1]] + d[l[3].rstrip(")")] + "010" + imm[7:] + "0100011"
