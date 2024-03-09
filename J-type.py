7aa41c3ad7872153d54a02402f782b076ca018ed
Ruhani05 <147978993+Ruhani05@users.noreply.github.com>
2024-03-09 23:32:21 +0530

    Create J-type.py

---
+++ J-type.py
@@ -0,0 +1,18 @@
+def decimal_to_twos_complement(decimal_num):
+    if decimal_num < 0:
+        abs_decimal = abs(decimal_num)
+        complement = 2**21 - abs_decimal
+    else:
+        complement = decimal_num
+
+    twos = bin(complement)[2:]
+    
+    if len(twos) < 21:
+        twos = '0' * (21 - len(twos)) + twos
+
+    return twos
+    
+
+def jal(l, d):
+    x=decimal_to_twos_complement(int(l[2]))
+    return x[0]+x[-11:-1:] + x[-12]+ x[-20:-12:]+ d[l[1]] + "1101111" 
