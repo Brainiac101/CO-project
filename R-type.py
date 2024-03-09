2036f3df5f0c2fb6d3836c3803be6d2f8ffe82d7
Lucifer1331 <162484243+Lucifer1331@users.noreply.github.com>
2024-03-09 14:50:26 +0530

    Update R-type.py

--- R-type.py
+++ R-type.py
@@ -40,6 +40,24 @@ def ory(l, dict):
 def andy(l, dict):
     return "0000000" + dict[l[3]] + dict[l[2]] + "111" + dict[l[1]] + "0110011";
 
+def beq(l, dict):
+    return " imm " + dict[l[2]] + dict[l[1]] + "000" + "imm part 2" + "1100011";
+
+def bne(l, dict):
+    return " imm " + dict[l[2]] + dict[l[1]] + "001" + "imm part 2" + "1100011";
+
+def blt(l, dict):
+    return " imm " + dict[l[2]] + dict[l[1]] + "100" + "imm part 2" + "1100011";
+
+def bge(l, dict):
+    return " imm " + dict[l[2]] + dict[l[1]] + "101" + "imm part 2" + "1100011";
+
+def bltu(l, dict):
+    return " imm " + dict[l[2]] + dict[l[1]] + "110" + "imm part 2" + "1100011";
+
+def bgeu(l, dict):
+    return " imm " + dict[l[2]] + dict[l[1]] + "111" + "imm part 2" + "1100011";
+
 dict={"zero" : "00000", "ra":"00001", "sp": "00010", "gp":"00011", "tp":"00100", "t0":"00101", "t1":"00110", "t2":"00111", "s0":"01000", "fp":"01000", "s1":"01001", "a0":"01010", "a1":"01011", "a2": "01100", "a3":"01101", "a4":"01110", "a5":"01111", "a6":"10000", "a7":"10001", "s2":"10010", "s3":"10011", "s4": "10100", "s5":"10101", "s6":"10110", "s7":"10111", "s8":"11000", "s9":"11001", "s10": "11010", "s11":"11011", "t3": "11100", "t4":"11101", "t5":"11110", "t6":"11111"}
 fin = open("sample.txt", "r")
 fout=open("output.txt", "w")
@@ -63,6 +81,18 @@ for line in fin:
         fout.write(ory(l, dict))
     elif l[0]=="and":
         fout.write(andy(l, dict))
+    elif l[0]=="beq":
+        fout.write(beq(l,dict))
+    elif l[0]=="bne":
+        fout.write(bne(l,dict))
+    elif l[0]=="bge":
+        fout.write(bge(l,dict))
+    elif l[0]=="bgeu":
+        fout.write(bgeu(l,dict))
+    elif l[0]=="blt":
+        fout.write(blt(l,dict))
+    elif l[0]=="bltu":
+        fout.write(bltu(l,dict))
     fout.write("\n")
 fin.close()
 fout.close()
