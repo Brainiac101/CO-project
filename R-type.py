5a93d73d00e1a5ff800affd831e41d8868532113
Lucifer1331 <162484243+Lucifer1331@users.noreply.github.com>
2024-03-10 12:01:14 +0530

    Update R-type.py

--- R-type.py
+++ R-type.py
@@ -52,50 +52,25 @@ def ory(l):
 def andy(l):
     global d;
     return "0000000" + d[l[3]] + d[l[2]] + "111" + d[l[1]] + "0110011";
-
-def beq(l):
-    global d;
-    return " imm " + d[l[2]] + d[l[1]] + "000" + "imm part 2" + "1100011";
-
-def bne(l):
-    global d;
-    return " imm " + d[l[2]] + d[l[1]] + "001" + "imm part 2" + "1100011";
-
-def blt(l):
-    global d;
-    return " imm " + d[l[2]] + d[l[1]] + "100" + "imm part 2" + "1100011";
-
-def bge(l):
-    global d;
-    return " imm " + d[l[2]] + d[l[1]] + "101" + "imm part 2" + "1100011";
-
-def bltu(l):
-    global d;
-    return " imm " + d[l[2]] + d[l[1]] + "110" + "imm part 2" + "1100011";
-
-def bgeu(l):
-    global d;
-    return " imm " + d[l[2]] + d[l[1]] + "111" + "imm part 2" + "1100011";
-
 d={"zero" : "00000", "ra":"00001", "sp": "00010", "gp":"00011", "tp":"00100", "t0":"00101", "t1":"00110", "t2":"00111", "s0":"01000", "fp":"01000", "s1":"01001", "a0":"01010", "a1":"01011", "a2": "01100", "a3":"01101", "a4":"01110", "a5":"01111", "a6":"10000", "a7":"10001", "s2":"10010", "s3":"10011", "s4": "10100", "s5":"10101", "s6":"10110", "s7":"10111", "s8":"11000", "s9":"11001", "s10": "11010", "s11":"11011", "t3": "11100", "t4":"11101", "t5":"11110", "t6":"11111"}
 labels={}
 fin = open("sample.txt", "r")
 fout = open("output.txt", "w")
 pc=0
-for line in fin:
+a=fin.readlines()
+for line in a:
     l=split(line);
-    while len(l)>4:
+    if len(l)>4:
         labels[l[0]]=pc*4;
     pc+=1;
 
 pc=0
-for line in fin:
+for line in a:
     l=split(line)
     if len(l)!=4:
-        labels[l[0]]=pc;
         l=l[1::];
-    d={"add":add(l),"sub":sub(l),"sll":sll(l),"slt":slt(l),"sltu":sltu(l),"xor":xor(l),"srl":srl(l),"or":ory(l),"and":andy(l),"beq":beq(l),"bne":bne(l),"bge":bge(l),"bgeu":bgeu(l),"blt":blt(l),"bltu":bltu(l)}
-    fout.write(d[l[0])
+    d1={"add":add(l),"sub":sub(l),"sll":sll(l),"slt":slt(l),"sltu":sltu(l),"xor":xor(l),"srl":srl(l),"or":ory(l),"and":andy(l)}
+    fout.write(d1[l[0])
     fout.write("\n")
     pc+=1;
 
