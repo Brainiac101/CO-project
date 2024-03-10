3118ccb8af83c68675e0136dea95aa34a51b166e
Lucifer1331 <162484243+Lucifer1331@users.noreply.github.com>
2024-03-10 13:03:23 +0530

    Update R-type.py

--- R-type.py
+++ R-type.py
@@ -17,40 +17,31 @@ def split(string):
     l.append(temp);
     return l;
 
-def add(l):
-    global d;
+def add(l,d):
     return "0000000" + d[l[3]] + d[l[2]] + "000" + d[l[1]] + "0110011";
 
-def sub(l):
-    global d;
+def sub(l,d):
     return "0100000" + d[l[3]] + d[l[2]] + "000" + d[l[1]] + "0110011";
     
-def sll(l):
-    global d;
+def sll(l,d):
     return "0000000" + d[l[3]] + d[l[2]] + "001" + d[l[1]] + "0110011";
 
-def slt(l):
-    global d;
+def slt(l,d):
     return "0000000" + d[l[3]] + d[l[2]] + "010" + d[l[1]] + "0110011";
 
-def sltu(l):
-    global d;
+def sltu(l,d):
     return "0000000" + d[l[3]] + d[l[2]] + "011" + d[l[1]] + "0110011";
 
-def xor(l):
-    global d;
+def xor(l,d):
     return "0000000" + d[l[3]] + d[l[2]] + "100" + d[l[1]] + "0110011";
 
-def srl(l):
-    global d;
+def srl(l,d):
     return "0000000" + d[l[3]] + d[l[2]] + "101" + d[l[1]] + "0110011";
 
-def ory(l):
-    global d;
+def ory(l,d):
     return "0000000" + d[l[3]] + d[l[2]] + "110" + d[l[1]] + "0110011";
 
-def andy(l):
-    global d;
+def andy(l,d):
     return "0000000" + d[l[3]] + d[l[2]] + "111" + d[l[1]] + "0110011";
 d={"zero" : "00000", "ra":"00001", "sp": "00010", "gp":"00011", "tp":"00100", "t0":"00101", "t1":"00110", "t2":"00111", "s0":"01000", "fp":"01000", "s1":"01001", "a0":"01010", "a1":"01011", "a2": "01100", "a3":"01101", "a4":"01110", "a5":"01111", "a6":"10000", "a7":"10001", "s2":"10010", "s3":"10011", "s4": "10100", "s5":"10101", "s6":"10110", "s7":"10111", "s8":"11000", "s9":"11001", "s10": "11010", "s11":"11011", "t3": "11100", "t4":"11101", "t5":"11110", "t6":"11111"}
 labels={}
@@ -69,7 +60,7 @@ for line in a:
     l=split(line)
     if len(l)!=4:
         l=l[1::];
-    d1={"add":add(l),"sub":sub(l),"sll":sll(l),"slt":slt(l),"sltu":sltu(l),"xor":xor(l),"srl":srl(l),"or":ory(l),"and":andy(l)}
+    d1={"add":add(l,d),"sub":sub(l,d),"sll":sll(l,d),"slt":slt(l,d),"sltu":sltu(l,d),"xor":xor(l,d),"srl":srl(l,d),"or":ory(l,d),"and":andy(l,d)}
     fout.write(d1[l[0])
     fout.write("\n")
     pc+=1;
