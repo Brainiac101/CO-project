d8152ff7baa4ddfe8b00cf557412b813b45469b5
Revant Gupta <147755345+Brainiac101@users.noreply.github.com>
2024-03-10 17:39:40 +0530

    Update btype.py

--- project/btype.py
+++ project/btype.py
@@ -1,17 +1,44 @@
-def beq(l, d):
-    return " imm " + d[l[2]] + d[l[1]] + "000" + "imm part 2" + "1100011";
+def decimal_to_twos_complement(decimal_num):
+    if decimal_num < 0:
+        abs_decimal = abs(decimal_num)
+        complement = 2**32 - abs_decimal
+    else:
+        complement = decimal_num
 
-def bne(l, d):
-    return " imm " + d[l[2]] + d[l[1]] + "001" + "imm part 2" + "1100011";
+    twos = bin(complement)[2:]
+    
+    if len(twos) < 32:
+        twos = '0' * (32 - len(twos)) + twos
 
-def blt(l, d):
-    return " imm " + d[l[2]] + d[l[1]] + "100" + "imm part 2" + "1100011";
+    return twos
+def beq(l,d,labels,pc):
+    if l[3] in labels:
+        print(l[3])
+        l[3]=pc*4-labels[l[3]];
+    print(decimal_to_twos_complement(int(l[3]))[19]+decimal_to_twos_complement(int(l[3]))[21:27] + d[l[2]] + d[l[1]] + "000" + decimal_to_twos_complement(int(l[3]))[27:31] + decimal_to_twos_complement(int(l[3]))[20]+ "1100011")
+    return decimal_to_twos_complement(int(l[3]))[19]+decimal_to_twos_complement(int(l[3]))[21:27] + d[l[2]] + d[l[1]] + "000" + decimal_to_twos_complement(int(l[3]))[27:31] + decimal_to_twos_complement(int(l[3]))[20]+ "1100011";
 
-def bge(l, d):
-    return " imm " + d[l[2]] + d[l[1]] + "101" + "imm part 2" + "1100011";
+def bne(l,d,labels,pc):
+    if l[3] in labels:
+        l[3]=pc*4-labels[l[3]];
+    return  decimal_to_twos_complement(int(l[3]))[19]+decimal_to_twos_complement(int(l[3]))[21:27]+ d[l[2]] + d[l[1]] + "001" +  decimal_to_twos_complement(int(l[3]))[27:31] + decimal_to_twos_complement(int(l[3]))[20] + "1100011";
 
-def bltu(l, d):
-    return " imm " + d[l[2]] + d[l[1]] + "110" + "imm part 2" + "1100011";
+def blt(l,d,labels,pc):
+    if l[3] in labels:
+        l[3]=pc*4-labels[l[3]];
+    return decimal_to_twos_complement(int(l[3]))[19]+decimal_to_twos_complement(int(l[3]))[21:27] + d[l[2]] + d[l[1]] + "100" +  decimal_to_twos_complement(int(l[3]))[27:31] + decimal_to_twos_complement(int(l[3]))[20] + "1100011";
 
-def bgeu(l, d):
-    return " imm " + d[l[2]] + d[l[1]] + "111" + "imm part 2" + "1100011";
+def bge(l,d,labels,pc):
+    if l[3] in labels:
+        l[3]=pc*4-labels[l[3]];
+    return decimal_to_twos_complement(int(l[3]))[19]+decimal_to_twos_complement(int(l[3]))[21:27] + d[l[2]] + d[l[1]] + "101" +  decimal_to_twos_complement(int(l[3]))[27:31] + decimal_to_twos_complement(int(l[3]))[20] + "1100011";
+
+def bltu(l,d,labels,pc):
+    if l[3] in labels:
+        l[3]=pc*4-labels[l[3]];
+    return decimal_to_twos_complement(int(l[3]))[19]+decimal_to_twos_complement(int(l[3]))[21:27] + d[l[2]] + d[l[1]] + "110" +  decimal_to_twos_complement(int(l[3]))[27:31] + decimal_to_twos_complement(int(l[3]))[20] + "1100011";
+
+def bgeu(l,d,labels,pc):
+    if l[3] in labels:
+        l[3]=pc*4-labels[l[3]];
+    return decimal_to_twos_complement(int(l[3]))[19]+decimal_to_twos_complement(int(l[3]))[21:27] + d[l[2]] + d[l[1]] + "111" +  decimal_to_twos_complement(int(l[3]))[27:31] + decimal_to_twos_complement(int(l[3]))[20] + "1100011";
