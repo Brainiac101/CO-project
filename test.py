78bd1a8e8c4fd047f80bd73e5c06d6cd2ac7bc59
Lucifer1331 <162484243+Lucifer1331@users.noreply.github.com>
2024-03-10 12:08:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,36 @@
+def split(string):
+    temp="";
+    l=[];
+    for i in string:
+        if i == " " or i == "," or i == ":"  or i=="(":
+            if temp=="":
+                continue;
+
+            l.append(temp);
+            temp="";
+            continue;
+
+        else:
+            temp+=i;
+    if temp[-1]=="\n":
+        temp=temp[:-1]
+    l.append(temp);
+    return l;
+d={"zero" : "00000", "ra":"00001", "sp": "00010", "gp":"00011", "tp":"00100", "t0":"00101", "t1":"00110", "t2":"00111", "s0":"01000", "fp":"01000", "s1":"01001", "a0":"01010", "a1":"01011", "a2": "01100", "a3":"01101", "a4":"01110", "a5":"01111", "a6":"10000", "a7":"10001", "s2":"10010", "s3":"10011", "s4": "10100", "s5":"10101", "s6":"10110", "s7":"10111", "s8":"11000", "s9":"11001", "s10": "11010", "s11":"11011", "t3": "11100", "t4":"11101", "t5":"11110", "t6":"11111"}
+labels={}
+fin = open("sample.txt", "r")
+fout = open("output.txt", "w")
+pc=0
+a=fin.readlines()
+for line in a:
+    l=split(line);
+    if len(l)>4:
+        labels[l[0]]=pc*4;
+    pc+=1;
+
+pc=0
+for line in a:
+    l=split(line)
+    if len(l)!=4:
+        l=l[1::];
 
