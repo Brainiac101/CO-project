05a52da8a73e9bc00f3ee4e8743ec8243b449040
Jaitrika <141649577+Jaitrika@users.noreply.github.com>
2024-03-11 15:08:50 +0530

    Update itype.py

--- project/itype.py
+++ project/itype.py
@@ -1,4 +1,4 @@
-def decimal_to_twos_complement(decimal_num):
+def decimal_to_twos_complement_12(decimal_num):
     if decimal_num < 0:
         abs_decimal = abs(decimal_num)
         complement = 2**12 - abs_decimal
@@ -9,21 +9,28 @@ def decimal_to_twos_complement(decimal_num):
     
     if len(twos) < 12:
         twos = '0' * (12 - len(twos)) + twos
+    
     return twos
+
     
 def lw(l,d):
     if abs(int(l[2]))>=2**11:
         return "0"
-    return decimal_to_twos_complement(int(l[2])) + d[l[3].rstrip(")")] + "010" + d[l[1]] + "0000011"
+    return decimal_to_twos_complement_12(int(l[2])) + d[l[3].rstrip(")")] + "010" + d[l[1]] + "0000011"
 def addi(l,d):
     if abs(int(l[3]))>=2**11:
         return "0"
-    return decimal_to_twos_complement(int(l[3])) + d[l[2]] + "000" + d[l[1]] + "0010011"
+    return decimal_to_twos_complement_12(int(l[3])) + d[l[2]] + "000" + d[l[1]] + "0010011"
 def sltiu(l,d):
     if abs(int(l[3]))>=2**11:
         return "0"
-    return decimal_to_twos_complement(int(l[3])) + d[l[2]] + "011" + d[l[1]] + "0010011"
-def jalr(l,d):
-    if abs(int(l[3]))>=2**11:
-        return "0"
-    return decimal_to_twos_complement(int(l[3])) + d[l[2]] + "000" + d[l[1]] + "1100111"
+    return decimal_to_twos_complement_12(int(l[3])) + d[l[2]] + "011" + d[l[1]] + "0010011"
+def jalr(l,d, labels, pc):
+    if l[3] in labels:
+        x=decimal_to_twos_complement_12(pc-labels[l[3]])
+    else:
+        x=decimal_to_twos_complement_12(int(l[3]))
+        if abs(int(l[3]))>=2**11:
+            return "0"
+    return x + d[l[2]] + "000" + d[l[1]] + "1100111"
+    
