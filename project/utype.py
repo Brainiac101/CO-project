5aaf47f160b85df5e859059b436d920b770a14ed
Revant Gupta <147755345+Brainiac101@users.noreply.github.com>
2024-03-11 23:57:06 +0530

    Update utype.py

--- project/utype.py
+++ project/utype.py
@@ -11,14 +11,19 @@ def decimal_to_twos_complement_32(decimal_num):
         twos = '0' * (32 - len(twos)) + twos
 
     return twos
-    
 
 def auipc(l,d):
+    x=decimal_to_twos_complement_32(int(l[2]))
+    if int(l[2])==-2**31:
+            return x[-32:-12:] + d[l[1]] + "0010111"
     if abs(int(l[2]))>=2**31:
         return "0"
     x=decimal_to_twos_complement_32(int(l[2]))
     return x[-32:-12:] + d[l[1]] + "0010111"
 def lui(l,d):
+    x=decimal_to_twos_complement_32(int(l[2]))
+    if (int(l[2]))==-2**31:
+            return x[-32:-12:] + d[l[1]] + "0110111"
     if abs(int(l[2]))>=2**31:
         return "0"
     x=decimal_to_twos_complement_32(int(l[2]))
