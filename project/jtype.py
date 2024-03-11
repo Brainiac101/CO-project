e9155f216dcce5ef14da994c497e1022f235f70d
Revant Gupta <147755345+Brainiac101@users.noreply.github.com>
2024-03-11 14:13:06 +0530

    Update jtype.py

--- project/jtype.py
+++ project/jtype.py
@@ -11,12 +11,12 @@ def decimal_to_twos_complement_21(decimal_num):
         twos = '0' * (21 - len(twos)) + twos
 
     return twos
-    
 
 def jal(l,d, labels, pc):
     if l[2] in labels:
-        x=decimal_to_twos_complement_21(pc-labels[l[2]])
+        x=decimal_to_twos_complement_21(labels[l[2]]-pc*4)
     else:
+        if abs(int(l[2]))>=2**20:
+            return "0"
         x=decimal_to_twos_complement_21(int(l[2]))
-    print(x,"<<")
     return x[0]+x[-11:-1:] + x[-12]+ x[-20:-12:]+ d[l[1]] + "1101111" 
