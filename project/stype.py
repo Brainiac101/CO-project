ab24716e1b246c26c7a51ae1f9dccca9954192f6
Revant Gupta <147755345+Brainiac101@users.noreply.github.com>
2024-03-11 14:14:50 +0530

    Update stype.py

--- project/stype.py
+++ project/stype.py
@@ -13,5 +13,7 @@ def decimal_to_twos_complement(decimal_num):
     return twos
 
 def sw(l,d):
+    if abs(int(l[2]))>=2**11:
+        return "0"
     imm=decimal_to_twos_complement(int(l[2]))
     return imm[:7]+ d[l[1]] + d[l[3].rstrip(")")] + "010" + imm[7:] + "0100011"
