0958bef02b5b5e0d6f5a3c3f1463c9130b202df2
Jaitrika <141649577+Jaitrika@users.noreply.github.com>
2024-04-17 02:09:35 +0530

    Create stype.py

---
+++ simulator/stype.py
@@ -0,0 +1,28 @@
+def bd(binary):
+    if binary[0] == '1':
+        inverted_binary = ''.join('1' if bit == '0' else '0' for bit in binary)
+        inverted_decimal = int(inverted_binary, 2) + 1
+        decimal = -inverted_decimal
+    else:
+        decimal = int(binary, 2)
+
+    return decimal
+
+def decimal_to_hexadecimal(decimal_num):
+    hexadecimal_num = hex(decimal_num)
+    return hexadecimal_num
+
+def sw(s,d,datamem):
+    imm=20*s[0]+s[-32:-25]+s[-12:-7]
+    mem=decimal_to_hexadecimal(bd(s[-20:-15])+(bd(imm)))
+    datamem[mem]=d[s[-25:-20]]
+    return datamem
+
+def check(s, d,datamem):
+    if s[17:20]=="010":
+        datamem=sw(s, d,datamem)
+
+
+
+
+
