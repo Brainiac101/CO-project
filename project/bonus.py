e24c0e54831f4b6d80f3db388b7d3a64222cb102
Ruhani05 <147978993+Ruhani05@users.noreply.github.com>
2024-04-17 01:46:38 +0530

    Update bonus.py

--- project/bonus.py
+++ project/bonus.py
@@ -1,1 +1,8 @@
-
+def mul(l,d):
+    return "0"*7+d[l[3]]+d[l[2]]+"0"*3+d[l[1]]+"0"*7
+def rst(l,d):
+    return "0"*32
+def halt(l,d):
+    return "0"*32
+def rvrs(l,d):
+    return "0"*12+d[l[2]]+"0"*3+d[l[1]]+"0"*7
