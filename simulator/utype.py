0341326a9fa352f00de5423bdeff5641046b4132
Ruhani05 <147978993+Ruhani05@users.noreply.github.com>
2024-04-17 01:32:52 +0530

    Update utype.py

--- simulator/utype.py
+++ simulator/utype.py
@@ -7,14 +7,27 @@ def binary_to_decimal(binary):
         decimal = int(binary, 2)
     return decimal
 
+def db(decimal_num):
+    if decimal_num < 0:
+        abs_decimal = abs(decimal_num)
+        complement = 2**32 - abs_decimal
+    else:
+        complement = decimal_num
+
+    twos = bin(complement)[2:]
+    
+    if len(twos) < 32:
+        twos = '0' * (32 - len(twos)) + twos
+
+    return twos
 
 
 def auipc(s,d,pc):
-    d[s[-12:-9:]]= pc + binary_to_decimal(s[-32:-12:]+'0'*12)
+    d[s[-12:-7:]]= db(pc + binary_to_decimal(s[-32:-12:]+'0'*12))
     return d
 
 def lui(s,d):
-    d[s[-12:-9:]]= binary_to_decimal(s[-32:-12:]+'0'*12)
+    d[s[-12:-7:]]= (s[-32:-12:]+'0'*12)
     return d
 
 def check(s,d,pc):
