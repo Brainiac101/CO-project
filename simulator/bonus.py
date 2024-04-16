6bdfed3f09022efb3787223dd5c031428339f39d
Revant Gupta <147755345+Brainiac101@users.noreply.github.com>
2024-04-17 00:49:42 +0530

    Create bonus.py

---
+++ simulator/bonus.py
@@ -0,0 +1,17 @@
+def rst():
+    pass
+    #jaldi banado
+def halt():
+    pass
+    #jaldi banado
+    
+def reverse(rs,rd,d):
+    d[rs]=d[rd][::-1]
+def check(s,d,pc):
+    if s[0:6]=='111111':
+        rst()
+    elif s[0:6]=='000000':
+        halt()
+    elif s[0:6]=='101010':
+        d=reverse(s[7:12],s[12:17],d)
+        return d
