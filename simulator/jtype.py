554d9523fc412599e72199d54e0f56ad1ba1cfe8
Ruhani05 <147978993+Ruhani05@users.noreply.github.com>
2024-04-17 01:33:17 +0530

    Update jtype.py

--- simulator/jtype.py
+++ simulator/jtype.py
@@ -7,9 +7,22 @@ def binary_to_decimal(binary):
         decimal = int(binary, 2)
     return decimal
 
+def db(decimal_num):
+    if decimal_num < 0:
+        abs_decimal = abs(decimal_num)
+        complement = 2**32 - abs_decimal
+    else:
+        complement = decimal_num
+
+    twos = bin(complement)[2:]
+    
+    if len(twos) < 32:
+        twos = '0' * (32 - len(twos)) + twos
+
+    return twos
 
 def jal(s,d,pc):
-    d[s[-12:-9:]]= pc + 4
+    d[s[-12:-7:]]= db(pc + 4)
     pc+= binary_to_decimal(s[0]+s[-20:-12:]+s[-21]+s[-31:-21:]+'0')
     return d,pc
 
