74e983c7f2911b0265539bfc186ddf92d1a9e680
Revant Gupta <147755345+Brainiac101@users.noreply.github.com>
2024-04-17 23:21:38 +0530

    Update bonus.py

--- project/bonus.py
+++ project/bonus.py
@@ -1,7 +1,6 @@
-
-def rst(l,d):
-    return "0000001" +"0"*25
-def halt(l,d):
+def rst():
+    return "0000001"+"0"*25
+def halt():
     return "0000010"+"0"*25
 def rvrs(l,d):
-    return "0000100"+ "0"*5+d[l[2]]+"0"*3+d[l[1]]+"0"*7
+    return "0000100"+"0"*5+d[l[2]]+"0"*3+d[l[1]]+"0"*7
