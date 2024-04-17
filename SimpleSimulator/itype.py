734fcee61e84c1a0a41998e4e6f0c864c4277670
Jaitrika <141649577+Jaitrika@users.noreply.github.com>
2024-04-18 02:54:14 +0530

    Update itype.py

--- SimpleSimulator/itype.py
+++ SimpleSimulator/itype.py
@@ -30,18 +30,18 @@ def decimal_to_hexadecimal(decimal_num):
     hexnum = '0x' + hexnum
     return hexnum
 
-def lw(s,d,datamem):
-    imm=20*s[0]+s[-32:-20]
-    mem=decimal_to_hexadecimal(bd(s[-20:-15])+(bd(imm)))
-    d[s[-12:7]]=datamem[mem]
+def lw(s,d,datamem, pc):
+    imm=s[-32:-20]
+    mem=decimal_to_hexadecimal(bd(d[s[12:17]])+(bd(imm)))
+    d[s[-12:-7]]=datamem[mem]
     return d
 
-def addi(s,d):
-    imm=20*s[0]+s[-32:-20]
+def addi(s,d, pc):
+    imm=s[-32:-20]
     d[s[-12:-7]]=db(bd(d[s[-20:-15]])+bd(imm))
     return d
 
-def sltiu(s,d):
+def sltiu(s,d, pc):
     if bidu(d[s[-20:-15]])<bidu(s[-32:-20]):
         d[s[19:24]]=db(1)
     return d
@@ -49,22 +49,18 @@ def sltiu(s,d):
 def jalr(s,d,pc):
     imm=20*s[0]+s[-32:-20]
     d[s[-12:-7:]]= db(pc + 4)
-    pc=bd(d[s[-20:-15]])+bd(imm)
+    pc=(bd(d[s[-20:-15]])+bd(imm))
     x=db(pc)
-    x[-1]=0 
-    pc=bd(x)
-    return d,pc
+    y=x[:-1]+'0'
+    pc=bd(y)
+    return d, pc
     
 def check(s,d,pc,datamem):
     if s[17:20] == "010":
-       return lw(s,d,pc,datamem)
-    elif s[17:20] == "000":
-       return addi(s,d)
+        return lw(s,d,datamem, pc)
+    elif s[17:20] == "000" and s[25:32]!="1100111":
+        return addi(s, d, pc)
     elif s[17:20] == "011":
-       return sltiu(s,d)
-    elif s[17:20] == "000" and s[0:7]=="1100111":
+       return sltiu(s,d, pc)
+    elif s[17:20] == "000" and s[25:32]=="1100111":
        return jalr(s,d,pc)
-    
-
-
-
